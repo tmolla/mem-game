98d641817d99dab716565fad1b20e148064b0d80
Telahoun Molla <telahounmolla@gmail.com>
2019-06-13 16:20:30 -0700

    Updates

---
+++ precache-manifest.031c5768581e7282d19b9832a938e45d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8c307cda5e1b3aacc63a05b3cf51b7",
+    "url": "/mem-game/index.html"
+  },
+  {
+    "revision": "3d7583348f9dda3f5133",
+    "url": "/mem-game/static/css/main.bebde47f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/mem-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3d7583348f9dda3f5133",
+    "url": "/mem-game/static/js/main.df517cb8.chunk.js"
+  },
+  {
+    "revision": "119dd813c60e3e6edfc8",
+    "url": "/mem-game/static/js/runtime~main.ba3ba4bf.js"
+  }
+]);
